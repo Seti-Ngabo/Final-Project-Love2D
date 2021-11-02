f90f023cee1be87686cc8e591596d4917d42860d
Seti-Ngabo <90275806+Seti-Ngabo@users.noreply.github.com>
2021-11-02 16:40:38 +0000

    added file

--- main.lua
+++ main.lua
@@ -1,3 +1,36 @@
+WINDOW_WIDTH = 620
+WINDOW_HEIGHT = 300
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
+--[[
+  Runs when the game first starts up, only once; used to initialzw the game.
+]]
+function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    vsync = true,
+    resizable = false
+  })
+end
+
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
+  end
+end
+--[[
+  Called after update by LOVE, used to draw anything to the screen, update or otherwise.
+]]
+
 function love.draw()
-    love.graphics.print("Hello World", 350, 250)
+  push:apply('start')
+
+    love.graphics.printf("Hello Pong!", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+  push:apply('end')
 end
