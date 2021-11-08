44600ff1f73fe8b62626ad6e6b89fa6c05d859c1
Seti-Ngabo <90275806+Seti-Ngabo@users.noreply.github.com>
2021-11-08 17:37:21 +0000

    added file

--- main.lua
+++ main.lua
@@ -1,16 +1,36 @@
+--[[
+  This is CS50 2019.
+  Game Track
+  Pong
+
+  pong-1
+  "The Low-Res Update"
+]]
+
+-- push is a library that will allow us to draw our game at a virtual
+-- resolution, instead of however large our window is; used to provide a more retro asethetic
+-- https://github.com/Ulydev/push
+
+push = require 'push'
+
 WINDOW_WIDTH = 620
 WINDOW_HEIGHT = 300
 
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
-push = require 'push'
-
 --[[
   Runs when the game first starts up, only once; used to initialzw the game.
 ]]
 function love.load()
+
+  -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text
+  -- and graphics; try removing this function to see the difference!
   love.graphics.setDefaultFilter('nearest', 'nearest')
+
+  smallFont = love.graphics.newFont('font.ttf', 8)
+  love.graphics.setFont(smallFont)
+
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
     vsync = true,
@@ -28,9 +48,18 @@ end
 ]]
 
 function love.draw()
+
+  -- begin rendering at virtual resolution
   push:apply('start')
 
-    love.graphics.printf("Hello Pong!", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+  love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
+
+  love.graphics.rectangle('fill', 5, 20, 5, 20)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 40, 5, 20)
+
+  love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
   push:apply('end')
 end
